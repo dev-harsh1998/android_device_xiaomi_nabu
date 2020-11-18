f23d0272d5c43eca2db112824284d9ab0eff1414
Alexander Koskovich <zvnexus@outlook.com>
2020-11-18 15:10:31 -0700

    nabu: Bring back the new AIDL QTI thermal HAL
    - Let's build it again to fix:
    E/ThermalHalWrapper( 1322): Thermal HAL 2.0 service not connected.
    E/ThermalHalWrapper( 1322): Thermal HAL 1.1 service not connected.
    E/ThermalHalWrapper( 1322): Thermal HAL 1.0 service not connected.
    W/ThermalManagerService( 1322): No Thermal HAL service on this device
    
    Signed-off-by: saikiran2001 <bjsaikiran@gmail.com>

--- device.mk
+++ device.mk
@@ -405,6 +405,10 @@ PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \
     hardware/xiaomi
 
+# Thermal
+PRODUCT_PACKAGES += \
+    android.hardware.thermal@2.0-service.qti
+
 # Trust
 PRODUCT_PACKAGES += \
     vendor.lineage.trust@1.0-service
