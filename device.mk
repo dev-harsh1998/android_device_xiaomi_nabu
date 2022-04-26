24b0f12b498f6f1709b2acc363e30fe7550a9231
SamarV-121 <samarvispute121@pm.me>
2022-04-25 20:56:35 -0400

    nabu: Uprev audio HAL to 7.0
    Change-Id: I4450ffa0337d5b10b9448093fd7abe4da85a463c
    Signed-off-by: saikiran2001 <bjsaikiran@gmail.com>

--- device.mk
+++ device.mk
@@ -106,8 +106,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # Audio
 PRODUCT_PACKAGES += \
-    android.hardware.audio@6.0-impl \
-    android.hardware.audio.effect@6.0-impl \
+    android.hardware.audio@7.0-impl \
+    android.hardware.audio.effect@7.0-impl \
     android.hardware.audio.service \
     android.hardware.soundtrigger@2.2-impl
 
