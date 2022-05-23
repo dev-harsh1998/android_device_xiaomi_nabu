9562ca86be5d0f987147e2d45d9cee0da2d6ccce
Bruno Martins <bgcngm@gmail.com>
2022-05-23 11:26:37 +0545

    nabu: Build power-off charging daemon dependency
    This must be explicitly built as of R. Fixes off mode charging.
    Change-Id: Ie013d25d55105b4eb2191b9fd625ed8e02c863c5

--- device.mk
+++ device.mk
@@ -167,6 +167,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     disable_configstore
 
+# Charger
+PRODUCT_PACKAGES += \
+    libsuspend
+
 # Component overrides
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/component-overrides.xml:$(TARGET_COPY_OUT_VENDOR)/etc/sysconfig/component-overrides.xml
