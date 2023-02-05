51ab3c11c4ee837e4cb57f730ddca517c2170ebb
Harshit Jain <dev-harsh1998@hotmail.com>
2023-02-05 01:33:29 +0000

    nabu: parts: add support for tap2wake using touchfeature
    * For now until I add support for kernel node after cleaning up the kernel
    
    Signed-off-by: Harshit Jain <dev-harsh1998@hotmail.com>

---
+++ parts/src/org/lineageos/settings/tap2wake/Tap2WakeSettingsFragment.java
@@ -0,0 +1,80 @@
+/*
+ * Copyright (C) 2018,2020 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.lineageos.settings.tap2wake;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Switch;
+
+import androidx.preference.ListPreference;
+import androidx.preference.Preference;
+import androidx.preference.Preference.OnPreferenceChangeListener;
+import androidx.preference.PreferenceFragment;
+import androidx.preference.SwitchPreference;
+import com.android.settingslib.widget.MainSwitchPreference;
+
+import vendor.xiaomi.hardware.touchfeature.V1_0.ITouchFeature;
+
+import org.lineageos.settings.R;
+
+public class Tap2WakeSettingsFragment extends PreferenceFragment implements
+OnPreferenceChangeListener {
+
+     private static final String TAP2WAKE_KEY = "tap2wake_switch_key";
+     public static final String SHARED_TAP2WAKE = "shared_tap2wake";
+
+     private SwitchPreference mTap2WakePreference;
+     private ITouchFeature mTouchFeature;
+
+     @Override
+     public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+         addPreferencesFromResource(R.xml.tap2wake_settings);
+
+         try {
+             mTouchFeature = ITouchFeature.getService();
+         } catch (Exception e) {
+             Log.e("TAP2WAKE", "Failed to get ITouchFeature service", e);
+         }
+         mTap2WakePreference = (SwitchPreference) findPreference(TAP2WAKE_KEY);
+         mTap2WakePreference.setEnabled(true);
+         mTap2WakePreference.setOnPreferenceChangeListener(this);
+     }
+
+     @Override
+     public boolean onPreferenceChange(Preference preference, Object newValue) {
+         if (TAP2WAKE_KEY.equals(preference.getKey())) {
+            enableTap2Wake((Boolean) newValue ? 1 : 0);
+         }
+         return true;
+     }
+
+     private void enableTap2Wake(int status) {
+         if (mTouchFeature == null)
+             return;
+         try {
+             mTouchFeature.setTouchMode(14, status);
+             SharedPreferences preferences = getActivity().getSharedPreferences(SHARED_TAP2WAKE, Context.MODE_PRIVATE);
+             SharedPreferences.Editor editor = preferences.edit();
+             editor.putInt(SHARED_TAP2WAKE, status);
+             editor.commit();
+         } catch (Exception e) {
+             Log.e("TAP2WAKE", "Failed to set tap2wake mode", e);
+         }
+     }
+}
--- parts/src/org/lineageos/settings/utils/PeripheralUtils.java
+++ parts/src/org/lineageos/settings/utils/PeripheralUtils.java
@@ -12,6 +12,7 @@ import static org.lineageos.settings.keyboard.XiaomiKeyboardSettingsFragment.SHA
 import static org.lineageos.settings.keyboard.XiaomiKeyboardSettingsFragment.KEYBOARDPATH;
 import static org.lineageos.settings.keyboard.XiaomiKeyboardSettingsFragment.KEYBOARDEN;
 import static org.lineageos.settings.keyboard.XiaomiKeyboardSettingsFragment.KEYBOARDDI;
+import static org.lineageos.settings.tap2wake.Tap2WakeSettingsFragment.SHARED_TAP2WAKE;
 
 public class PeripheralUtils {
     private static final String TAG = "PeripheralUtils";
@@ -19,12 +20,14 @@ public class PeripheralUtils {
     private static ITouchFeature mTouchFeature;
     private static SharedPreferences stylus;
     private static SharedPreferences keyboard;
+    private static SharedPreferences tap2wake;
 
     public static void BootResetState(Context context) {
         if (DEBUG) Log.d(TAG, "Starting service");
         // Initialize stylus and keyboard shared preferences
         stylus = context.getSharedPreferences(SHARED_STYLUS, Context.MODE_PRIVATE);
         keyboard = context.getSharedPreferences(SHARED_KEYBOARD, Context.MODE_PRIVATE);
+        tap2wake = context.getSharedPreferences(SHARED_TAP2WAKE, Context.MODE_PRIVATE);
 
         // try to get the touchfeature service.
         try {
@@ -60,9 +63,22 @@ public class PeripheralUtils {
         }
     }
 
+    // Enable tap2wake based on shared preference.
+    public static void SyncTap2Wake() {
+        if (DEBUG) Log.d(TAG, "Enabling tap2wake");
+        if (mTouchFeature != null) {
+            try {
+                mTouchFeature.setTouchMode(14, tap2wake.getInt(SHARED_TAP2WAKE, 0));
+            } catch (Exception e) {
+                Log.e(TAG, "Failed to enable tap2wake", e);
+            }
+        }
+    }
+
     // Sync all peripherals
     private static void SyncAll() {
         SyncStylus();
         SyncKeyboard();
+        SyncTap2Wake();
     }
 }
