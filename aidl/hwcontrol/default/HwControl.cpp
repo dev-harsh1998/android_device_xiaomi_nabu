feea9b37efc0c391734400a2d858dfa30d85a461
Harshit Jain <dev-harsh1998@hotmail.com>
2023-02-18 02:49:52 +0530

    nabu: hwcontrol: use modern logging macros everywhere

--- aidl/hwcontrol/default/HwControl.cpp
+++ aidl/hwcontrol/default/HwControl.cpp
@@ -3,7 +3,7 @@
 #include "HwControl.h"
 
 #include <android/binder_manager.h>
-#include <log/log.h>
+#include <android-base/logging.h>
 
 #include <algorithm>
 #include <cassert>
@@ -16,16 +16,16 @@
 namespace aidl::custom::hardware::hwcontrol {
 
 ::ndk::ScopedAStatus HwControl::getHwState(HwType hwType, int32_t* _aidl_return) {
-    ALOGD("getHwState");
+    LOG(INFO) << LOG_TAG << ": " << "getHwState: " << hwType;
     // Switch HW type
     switch (hwType) {
         case HwType::KEYBOARD:
             *_aidl_return = getKeyboardState();
-            ALOGI("getHwState: Keyboard state is %d", *_aidl_return);
+            LOG(INFO) << LOG_TAG << ": " << "getHwState: Keyboard state is " << *_aidl_return;
             break;
         default:
             *_aidl_return = 0;
-            ALOGE("getHwState: Unimplemented HW type");
+            LOG(ERROR) << LOG_TAG << ": " << "getHwState: Unimplemented HW type";
             break;
     }
     return ::ndk::ScopedAStatus::ok();
@@ -33,16 +33,16 @@ namespace aidl::custom::hardware::hwcontrol {
 }
 
 ::ndk::ScopedAStatus HwControl::setHwState(HwType hwType, int32_t state) {
-    ALOGD("setHwState");
+    LOG(INFO) << LOG_TAG << ": " << "setHwState: " << hwType << " to " << state;
     switch (hwType) {
         case HwType::KEYBOARD:
             // for keyboard we only accept 0 or 1, consider anything greater than 1 as 1
             (state > 1) ? 1 : state;
             setKeyboardState(state);
-            ALOGI("setHwState: Keyboard state set to %d", state);
+            LOG(INFO) << LOG_TAG << ": " << "setHwState: Keyboard state set to " << state;
             break;
         default:
-            ALOGE("setHwState: Unimplemented HW type");
+            LOG(ERROR) << LOG_TAG << ": " << "setHwState: Unimplemented HW type";
             break;
     }
     return ::ndk::ScopedAStatus::ok();
@@ -50,7 +50,7 @@ namespace aidl::custom::hardware::hwcontrol {
 
 // Base constructor
 HwControl::HwControl(void) {
-    ALOGD("HwControl::HwControl constructor initialized");
+    LOG(INFO) << LOG_TAG << ": " << "HwControl: Constructor";
 }
 
 }
