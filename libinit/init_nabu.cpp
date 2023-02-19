316f6663eab9638d4b06ed2db84516fd68d93c28
Harshit Jain <dev-harsh1998@hotmail.com>
2023-02-20 02:45:46 +0530

    nabu: init: set build description as well as build fingerprint
    * Reformat the file and arrange the code a bit while we are at it
    
    Signed-off-by: Harshit Jain <dev-harsh1998@hotmail.com>

--- libinit/init_nabu.cpp
+++ libinit/init_nabu.cpp
@@ -18,8 +18,18 @@
 #include "property_service.h"
 #include "vendor_init.h"
 
+struct device_variant
+{
+  const std::string model;
+  const std::string device_name;
+  const std::string build_fingerprint;
+  const std::string build_description;
+};
 
-void property_override(char const prop[], char const value[], bool add = true) {
+using dv = device_variant;
+
+void property_override(char const prop[], char const value[], bool add = true)
+{
   prop_info *pi;
   pi = (prop_info *)__system_property_find(prop);
   if (pi)
@@ -28,38 +38,49 @@ void property_override(char const prop[], char const value[], bool add = true) {
     __system_property_add(prop, strlen(prop), value, strlen(value));
 }
 
-void set_device_properties(const std::string model, const std::string name,
-                           const std::string marketname) {
+void set_device_properties(const dv &variant)
+{
   const std::array<std::string, 7> props_order = {
-      std::string(""), "bootimage.",  "odm.",   "product.",
-      "system.",       "system_ext.", "vendor."};
+      std::string(""), "bootimage.", "odm.", "product.",
+      "system.", "system_ext.", "vendor."};
 
   const auto set_ro_build_prop = [](
-      const std::string &source, const std::string &prop,
-      const std::string &value, bool product = true) {
+                                     const std::string &source, const std::string &prop,
+                                     const std::string &value, bool product = true)
+  {
     property_override(
         std::string((product) ? std::string("ro.product." + source + prop)
                               : std::string("ro." + source + prop))
             .c_str(),
         value.c_str());
   };
-  for (auto &props : props_order) {
-    set_ro_build_prop(props, "model", model);
-    set_ro_build_prop(props, "name", name);
-    set_ro_build_prop(props, "marketname", marketname);
+
+  for (auto &props : props_order)
+  {
+    set_ro_build_prop(props, "fingerprint", variant.build_fingerprint, false);
+    set_ro_build_prop(props, "model", variant.model);
+    set_ro_build_prop(props, "name", variant.device_name);
+    // common for all variants is the market name.
+    set_ro_build_prop(props, "marketname", "Xiaomi Pad 5");
   }
-  property_override("bluetooth.device.default_name", model.c_str());
-  property_override("vendor.usb.product_string", model.c_str());
+
+  property_override("ro.build.description", variant.build_description.c_str());
+  property_override("bluetooth.device.default_name", variant.model.c_str());
+  property_override("vendor.usb.product_string", variant.model.c_str());
 }
 
-void witch_nabu() {
-  if ((::android::base::GetProperty("ro.boot.hwc", std::string("GLOBAL")) ==
-       "GLOBAL")) {
-    set_device_properties("21051182G", "nabu_global", "Xiaomi Pad 5");
-  }
-  else {
-    set_device_properties("21051182C", "nabu_cn", "Xiaomi Pad 5");
-  }
+void witch_nabu()
+{
+
+  dv variants[2] = {
+      {"21051182G", "nabu_global", "Xiaomi/nabu_global/nabu:12/RKQ1.200826.002/V13.1.4.0.SKXMIXM:user/release-keys",
+       "nabu_global-user 12 SKQ1.220303.001 V13.1.4.0.SKXMIXM release-keys"},
+      {"21051182C", "nabu", "Xiaomi/nabu/nabu:13/RKQ1.200826.002/V14.0.4.0.TKXCNXM:user/release-keys",
+       "nabu-user 13 TKQ1.221013.002 V14.0.4.0.TKXCNXM release-keys"},
+  };
+
+  set_device_properties(((::android::base::GetProperty("ro.boot.hwc", std::string("GLOBAL")) == "GLOBAL")) ? variants[0] : variants[1]);
+
   property_override("ro.boot.hardware.revision", ::android::base::GetProperty("ro.boot.hwversion", "").c_str());
 }
 
