92aa8b6013449cf8c9247591e3ca638ab386f80e
Yan Yan <evitayan@google.com>
2023-04-12 22:56:19 +0000

    nabu: Add feature flag for IPSEC_TUNNEL_MIGRATION
    This commit adds the IPSEC_TUNNEL_MIGRATION feature flag,
    with XFRM_MIGRATE used as the implementation
    
    Bug: 273379472
    Test: build, flash and verify with "adb shell pm list features"
    Test: atest IpSecManagerTunnelTest
    Change-Id: Id2ee3e65183e32ea63f6251ba921e1e4c6b09ed0
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- device.mk
+++ device.mk
@@ -84,6 +84,7 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.wifi.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.wifi.xml \
     frameworks/native/data/etc/android.hardware.ethernet.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.ethernet.xml \
     frameworks/native/data/etc/android.software.ipsec_tunnels.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.ipsec_tunnels.xml \
+    frameworks/native/data/etc/android.software.ipsec_tunnel_migration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.ipsec_tunnel_migration.xml \
     frameworks/native/data/etc/android.software.midi.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.midi.xml \
     frameworks/native/data/etc/android.software.sip.voip.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.sip.voip.xml \
     frameworks/native/data/etc/handheld_core_hardware.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/handheld_core_hardware.xml \
