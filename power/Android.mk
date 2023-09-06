e7c5b891c1f18e7224061f4173280f88f8f60585
saikiran2001 <bjsaikiran@gmail.com>
2023-09-06 18:58:35 +0200

    [SQUASH] nabu: Import & Switch to QTI Powerhal
    Signed-off-by: saikiran2001 <bjsaikiran@gmail.com>

---
+++ power/Android.mk
@@ -0,0 +1,141 @@
+LOCAL_PATH := $(call my-dir)
+
+ifeq ($(call is-vendor-board-platform,QCOM),true)
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE_RELATIVE_PATH := hw
+
+LOCAL_SHARED_LIBRARIES := \
+    liblog \
+    libcutils \
+    libdl \
+    libbase \
+    libutils \
+    android.hardware.power-V1-ndk \
+    libbinder_ndk
+
+LOCAL_HEADER_LIBRARIES := \
+    libhardware_headers
+
+LOCAL_SRC_FILES := \
+    power-common.c \
+    metadata-parser.c \
+    utils.c \
+    list.c \
+    hint-data.c \
+    Power.cpp \
+    main.cpp
+
+LOCAL_CFLAGS += -Wall -Wextra -Werror
+
+ifneq ($(BOARD_POWER_CUSTOM_BOARD_LIB),)
+    LOCAL_WHOLE_STATIC_LIBRARIES += $(BOARD_POWER_CUSTOM_BOARD_LIB)
+else
+
+# Include target-specific files.
+ifeq ($(call is-board-platform-in-list,msm8960), true)
+LOCAL_SRC_FILES += power-8960.c
+endif
+
+ifeq ($(call is-board-platform-in-list,msm8974), true)
+LOCAL_SRC_FILES += power-8974.c
+endif
+
+ifeq ($(call is-board-platform-in-list,msm8226), true)
+LOCAL_SRC_FILES += power-8226.c
+endif
+
+ifeq ($(call is-board-platform-in-list,msm8610), true)
+LOCAL_SRC_FILES += power-8610.c
+endif
+
+ifeq ($(call is-board-platform-in-list,apq8084), true)
+LOCAL_SRC_FILES += power-8084.c
+endif
+
+ifeq ($(call is-board-platform-in-list,msm8909), true)
+LOCAL_SRC_FILES += power-8909.c
+endif
+
+ifeq ($(call is-board-platform-in-list,msm8916), true)
+LOCAL_SRC_FILES += power-8916.c
+endif
+
+ifeq ($(call is-board-platform-in-list,msm8992), true)
+LOCAL_SRC_FILES += power-8992.c
+endif
+
+ifeq ($(call is-board-platform-in-list,msm8994), true)
+LOCAL_SRC_FILES += power-8994.c
+endif
+
+ifeq ($(call is-board-platform-in-list,msm8996), true)
+LOCAL_SRC_FILES += power-8996.c
+endif
+
+ifeq ($(call is-board-platform-in-list,msm8937), true)
+LOCAL_SRC_FILES += power-8937.c
+endif
+
+ifeq ($(call is-board-platform-in-list,msm8952), true)
+LOCAL_SRC_FILES += power-8952.c
+endif
+
+ifeq ($(call is-board-platform-in-list,msm8953), true)
+LOCAL_SRC_FILES += power-8953.c
+endif
+
+ifeq ($(call is-board-platform-in-list,msm8998 apq8098_latv), true)
+LOCAL_SRC_FILES += power-8998.c
+endif
+
+ifeq ($(call is-board-platform-in-list,sdm660), true)
+LOCAL_SRC_FILES += power-660.c
+endif
+
+ifeq ($(call is-board-platform-in-list,sdm845), true)
+LOCAL_SRC_FILES += power-845.c
+endif
+
+ifeq ($(call is-board-platform-in-list,sdm710), true)
+LOCAL_SRC_FILES += power-710.c
+endif
+
+ifeq ($(call is-board-platform-in-list,qcs605), true)
+LOCAL_SRC_FILES += power-710.c
+endif
+
+ifeq ($(call is-board-platform-in-list,msmnile), true)
+LOCAL_SRC_FILES += power-msmnile.c
+endif
+
+endif # End of board specific list
+
+ifneq ($(TARGET_POWERHAL_MODE_EXT),)
+    LOCAL_CFLAGS += -DMODE_EXT
+    LOCAL_SRC_FILES += ../../../../$(TARGET_POWERHAL_MODE_EXT)
+endif
+
+ifneq ($(TARGET_POWERHAL_SET_INTERACTIVE_EXT),)
+    LOCAL_CFLAGS += -DSET_INTERACTIVE_EXT
+    LOCAL_SRC_FILES += ../../../../$(TARGET_POWERHAL_SET_INTERACTIVE_EXT)
+endif
+
+ifneq ($(TARGET_TAP_TO_WAKE_NODE),)
+    LOCAL_CFLAGS += -DTAP_TO_WAKE_NODE=\"$(TARGET_TAP_TO_WAKE_NODE)\"
+endif
+
+ifeq ($(TARGET_USES_INTERACTION_BOOST),true)
+    LOCAL_CFLAGS += -DINTERACTION_BOOST
+endif
+
+LOCAL_MODULE := android.hardware.power-service-nabu
+LOCAL_INIT_RC := android.hardware.power-service-nabu.rc
+LOCAL_MODULE_TAGS := optional
+LOCAL_CFLAGS += -Wno-unused-parameter -Wno-unused-variable
+LOCAL_VENDOR_MODULE := true
+LOCAL_VINTF_FRAGMENTS := power.xml
+
+include $(BUILD_EXECUTABLE)
+endif
