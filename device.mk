7a246e0478ee01086f5fc6da79df120f113a1a3e
Michael Bestas <mkbestas@lineageos.org>
2024-04-20 23:12:35 +0530

    nabu: Switch to android.hardware.usb@1.3-service.dual_role_usb

--- device.mk
+++ device.mk
@@ -416,7 +416,7 @@ PRODUCT_PACKAGES += \
 
 # USB
 PRODUCT_PACKAGES += \
-    android.hardware.usb@1.0-service
+    android.hardware.usb@1.3-service.dual_role_usb
 
 # Vendor service manager
 PRODUCT_PACKAGES += \
