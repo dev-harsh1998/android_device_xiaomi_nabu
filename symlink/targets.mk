9f65372037ba648f6fda500290c0b6bd66974508
Harshit Jain <reach@harsh1998.dev>
2025-03-24 00:17:17 +0530

    Revert "Revert "nabu: Convert WiFi firmware symlinks to install_symlink targets""
    This reverts commit ba12d97aaaa2002def6b777cae23f33b75164339.

--- symlink/targets.mk
+++ symlink/targets.mk
@@ -2,3 +2,7 @@
 $(warning "RFS Symlink inherited from hardware/qcom-caf/common/common.mk")
 $(call inherit-product, hardware/qcom-caf/common/common.mk)
 
+# Wlan symlinks
+PRODUCT_PACKAGES += \
+    firmware_WCNSS_qcom_cfg.ini_symlink \
+    firmware_wlan_mac.bin_symlink
