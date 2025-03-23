b907915013ce25b2225d171500af2a1d3604bfb7
Harshit Jain <reach@harsh1998.dev>
2025-03-24 00:16:56 +0530

    Revert "Revert "nabu: Move to new RFS install_symlink targets""
    This reverts commit 30a6ba8d537d552c79c29989536cd0a5e5fa8bcc.

---
+++ symlink/targets.mk
@@ -0,0 +1,4 @@
+# RFS Symlink inherited from hardware/qcom-caf/common/common.mk
+$(warning "RFS Symlink inherited from hardware/qcom-caf/common/common.mk")
+$(call inherit-product, hardware/qcom-caf/common/common.mk)
+
