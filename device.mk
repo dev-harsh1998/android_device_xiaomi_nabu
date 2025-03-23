6235bd85377e4f5f907557700eafdac28078495e
Harshit Jain <reach@harsh1998.dev>
2025-03-23 07:10:00 +0530

    Revert "nabu: Switch to common lights HAL"
    This reverts commit d3fe4fd6eef3b8ca2c5f233ceb19aeb6c1a3601c.

--- device.mk
+++ device.mk
@@ -293,7 +293,7 @@ PRODUCT_PACKAGES += \
 
 # Lights
 PRODUCT_PACKAGES += \
-    android.hardware.light-service.lineage
+    android.hardware.light-service.xiaomi
 
 # Media Configs
 PRODUCT_COPY_FILES += \
