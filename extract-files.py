ba2b8b121fcc6df67adc36fcc8fe2fd5a03f8d67
Harshit Jain <reach@harsh1998.dev>
2025-06-10 06:27:24 +0530

    nabu: extract-util: migrate thestuff from sh to py
    * Also remove the redundant BoardConfig flag.

--- extract-files.py
+++ extract-files.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env -S PYTHONPATH=../../../tools/extract-utils python3
 #
-# SPDX-FileCopyrightText: 2024 The LineageOS Project
+# SPDX-FileCopyrightText: 2025 The LineageOS Project
 # SPDX-License-Identifier: Apache-2.0
 #
 
@@ -35,14 +35,34 @@ lib_fixups: lib_fixups_user_type = {
     **lib_fixups,
     (
         'com.qualcomm.qti.dpm.api@1.0',
+        'libmmosal',
     ): lib_fixup_vendor_suffix,
 }
 
 blob_fixups: blob_fixups_user_type = {
+    'vendor/etc/init/init.batterysecret.rc': blob_fixup()
+        .regex_replace('.*seclabel u:r:batterysecret:s0\n', ''),
+    'vendor/etc/init/init.mi_thermald.rc': blob_fixup()
+        .regex_replace('.*seclabel u:r:mi_thermald:s0\n', ''),
+    'vendor/lib/hw/audio.primary.nabu.so': blob_fixup()
+        .binary_regex_replace(
+            b'/vendor/lib/liba2dpoffload.so',
+            b'liba2dpoffload_nabu.so\x00\x00\x00\x00\x00\x00\x00',
+        ),
     'vendor/lib64/camera/components/com.qti.node.watermark.so': blob_fixup()
         .add_needed("libpiex_shim.so"),
     ('vendor/lib64/mediadrm/libwvdrmengine.so', 'vendor/lib64/libwvhidl.so'): blob_fixup()
         .add_needed("libcrypto_shim.so"),
+    (
+        'vendor/lib64/libalAILDC.so',
+        'vendor/lib64/libalLDC.so',
+        'vendor/lib64/libalhLDC.so',
+    ): blob_fixup()
+        .clear_symbol_version('AHardwareBuffer_allocate')
+        .clear_symbol_version('AHardwareBuffer_describe')
+        .clear_symbol_version('AHardwareBuffer_lock')
+        .clear_symbol_version('AHardwareBuffer_release')
+        .clear_symbol_version('AHardwareBuffer_unlock'),
 }  # fmt: skip
 
 module = ExtractUtilsModule(
