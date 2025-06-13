1c89e561b456c19d10a7ba0c8d73b0f949111251
Michael Bestas <mkbestas@lineageos.org>
2025-06-13 05:59:37 +0530

    nabu: Migrate mount point creation out of Android.mk
    * Do we even need the GPU blob symlinks?

--- symlink/targets.mk
+++ symlink/targets.mk
@@ -2,8 +2,11 @@
 $(warning "RFS Symlink inherited from hardware/qcom-caf/common/common.mk")
 $(call inherit-product, hardware/qcom-caf/common/common.mk)
 
-$(warning "Will create wlan symlinks")
-# Wlan symlinks
+$(warning "Will create symlinks")
+# symlinks
 PRODUCT_PACKAGES += \
     firmware_WCNSS_qcom_cfg.ini_symlink \
-    firmware_wlan_mac.bin_symlink
+    firmware_wlan_mac.bin_symlink \
+    vendor_bt_firmware_mountpoint \
+    vendor_dsp_mountpoint \
+    vendor_firmware_mnt_mountpoint
