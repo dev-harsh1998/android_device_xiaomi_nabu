1abebce53a377da245e965740162081632f08fbe
Sebastiano Barezzi <seba@sebaubuntu.dev>
2025-06-13 06:14:37 +0530

    nabu: use hardware/xiaomi & qcom FCM
    Co-authored-by: Bruno Martins <bgcngm@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -178,10 +178,11 @@ BOARD_MOVE_RECOVERY_RESOURCES_TO_VENDOR_BOOT := true
 
 # VINTF
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
-    $(DEVICE_PATH)/framework_compatibility_matrix.xml \
+    hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml \
+    hardware/xiaomi/vintf/xiaomi_framework_compatibility_matrix.xml \
     vendor/lineage/config/device_framework_matrix.xml
-DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/manifest.xml
 DEVICE_MATRIX_FILE += hardware/qcom-caf/common/compatibility_matrix.xml
+DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/manifest.xml
 
 # Wi-Fi
 BOARD_WLAN_DEVICE := qcwcn
