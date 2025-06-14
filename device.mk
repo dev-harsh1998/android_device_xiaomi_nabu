e10736f47c054cbcfc6bda7008a9b25c26e9c120
Sebastiano Barezzi <seba@sebaubuntu.dev>
2025-06-14 05:31:40 +0530

    nabu: move fstab & remaining rc to soong
    Co-authored-by: SahilSonar <sss.sonar2003@gmail.com>

--- device.mk
+++ device.mk
@@ -368,13 +368,14 @@ PRODUCT_PACKAGES += \
     libvndfwk_detect_jni.qti \
     libvndfwk_detect_jni.qti.vendor
 
-# Filesystems table
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/rootdir/etc/fstab.qcom:$(TARGET_COPY_OUT_VENDOR)/etc/fstab.qcom \
-    $(LOCAL_PATH)/rootdir/etc/fstab.qcom:$(TARGET_COPY_OUT_RECOVERY)/root/first_stage_ramdisk/fstab.qcom \
-    $(LOCAL_PATH)/rootdir/etc/fstab.qcom:$(TARGET_COPY_OUT_VENDOR_RAMDISK)/first_stage_ramdisk/fstab.qcom
-
+# init
 $(call soong_config_set,libinit,vendor_init_lib,//$(LOCAL_PATH):init_nabu)
+
+# Rootdir
+PRODUCT_PACKAGES += \
+    fstab.qcom \
+    fstab.qcom.ramdisk \
+    fstab.qcom.vendor_ramdisk
 
 PRODUCT_PACKAGES += \
     init.class_main.sh \
