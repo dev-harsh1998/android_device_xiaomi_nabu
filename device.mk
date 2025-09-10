6b1ca36a367a0cfb9f7a97ba0629d40ddd22d5b1
ppanzenboeck <panz.development@gmail.com>
2025-09-10 17:15:43 +0200

    nabu: configs: declare hardware midi support
    requires: https://github.com/dev-harsh1998/android_kernel_xiaomi_sm8150/commit/bd15987312f9e6530970b51a7f5afdc9fef741d5
    
    Signed-off-by: Harshit Jain <reach@harsh1998.dev>

--- device.mk
+++ device.mk
@@ -99,6 +99,10 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.verified_boot.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.verified_boot.xml \
     $(LOCAL_PATH)/configs/android-removed-permissions.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android-removed-permissions.xml
 
+# Add external hardware midi support
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/android.hardware.midi.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.midi.xml
+
 # Device is a Tablet
 PRODUCT_CHARACTERISTICS := tablet,nosdcard
 TARGET_SCREEN_HEIGHT := 2560
