d07fd74888a894f9512447001711c4a2d831d6c9
Michael Bestas <mkbestas@lineageos.org>
2026-01-22 03:45:36 +0530

    nabu: patch libdpps.so to load qcom's libtinyxml2_1.so
    libtinyxml2 10.1.0 update which landed in BP4A contains ABI
    incompatible changes which cause runtime crashes.
    Since the lib lives in system, we can't easily blob it,
    however we can use qualcomm's libtinyxml2_1.so that already exists
    in vendor and seems to work as expected.
    
    [@dev-harsh1998]: Quick tip if you wish to find old libraries in
    the vendor partition that depend on the libtinyxml2 just run the
    following command in the cloned vendor repo inside proprietary folder.
    
    find vendor -name "*.so" -print0 | while IFS= read -r -d $'\0' file; do if readelf -d "$file" 2>/dev/null | grep -q  "libtinyxml2.so"; then echo "$file"; fi; done
    
    Change-Id: Icc67da5f754b0bb30dce6192b1e520fdf1349ac5

--- extract-files.py
+++ extract-files.py
@@ -40,6 +40,8 @@ lib_fixups: lib_fixups_user_type = {
 }
 
 blob_fixups: blob_fixups_user_type = {
+    'vendor/lib64/libdpps.so': blob_fixup()
+        .replace_needed('libtinyxml2.so', 'libtinyxml2_1.so'),
     'vendor/etc/init/init.batterysecret.rc': blob_fixup()
         .regex_replace('.*seclabel u:r:batterysecret:s0\n', ''),
     'vendor/etc/init/init.mi_thermald.rc': blob_fixup()
